27dd6702653c63acfec70c05b2f3281b96219ae1
Frogerall <saliticoadrian@gmail.com>
2023-11-27 02:12:24 +0800

    added mobile design for hero-section

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const navMenu = document.querySelector("nav");
+const navClose = document.querySelector(".nav-close");
+const navBtn = document.querySelector(".nav-menu");
 
-const navBtn = document
- .querySelector(".nav-menu")
- .addEventListener("click", () => {
-  navMenu.classList.toggle("nav-open");
- });
+const toggleNav = () => {
+ navMenu.classList.toggle("nav-open");
+};
+const closeNavOnResize = () => {
+ navMenu.classList.remove("nav-open");
+};
+navBtn.addEventListener("click", toggleNav);
+window.addEventListener("resize", closeNavOnResize);
